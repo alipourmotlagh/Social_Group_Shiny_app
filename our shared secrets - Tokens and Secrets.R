4d185c3da0cd8a05aad51e239ea97f96c7db7db8
alipourmotlagh <alipourmotlagh@gmail.com>
2022-01-30 22:54:13 +0100

    1

---
+++ our shared secrets - Tokens and Secrets.R
@@ -0,0 +1,28 @@
+#this is for our secret codes for using api,
+#it is better to have them here for twitter but manage how to use them wisely,
+#--------------------------------
+#          Jeanne
+#--------------------------------
+
+
+
+#--------------------------------
+#          Juan
+#--------------------------------
+# spotify
+
+
+
+#--------------------------------
+#          Hadi
+#--------------------------------
+
+
+Hadi_spotify_client_id<-'624156ff95ac47deb3183ade188675b2'
+Hadi_spotify_client_Secret<-'2283c88c83fb4b7ea59879ba8f2dede6'
+  
+  
+Hadi_Genius_client_id<-'BMcb4MCPS7wKzRsANcTGkmlrk6F9awFfutYdNcNrLuDcCtyyqujYnP2SVxxF9Tct'
+Hadi_Genius_client_Secret<-'EtndbJMM1nQ6fwnpEnWhoV0CtMhX3i7bbG44Rsg4Zh0G5Txd7xcxmZ8Bky6s8F7nRupg0NgQpN_Q9KZPYEwhzA'
+Hadi_Genius_client_Access<-'TNCORwp182PSwEGvYiOudM0XnobOYpJGEdq5TnR-d0BGiur9H3e39SWC36Wihj0w'
+
